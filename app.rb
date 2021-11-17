0e137e842c9111c891cc9d479e098456d29481ad
tdds <nyxa@nextmail.ru>
2021-11-18 05:50:42 +0700

    initplusdz

---
+++ app.rb
@@ -0,0 +1,90 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/contrib/all'
+require 'pony'
+require 'sqlite3'
+
+get '/' do
+  erb "<p>Hello Epta</p>"
+end
+
+get '/about' do
+  @error= 'ATAKA-ATAKA'
+  erb :about
+end
+
+get '/contacts' do
+  erb :contacts 
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+
+  @customer =params[:customer]
+  @date = params[:date]
+  @phone = params[:phone]
+  @barber = params[:barber]
+  @color = params[:color]
+    hh = {:customer=>'Enter your name', 
+        :phone=>'Enter your phone number', 
+        :date=>'Enter your arrival time', 
+        :color=>'Choose your color'}
+
+        @error = hh.select {|key, value| params[key]==""}.values.join(", ")
+      if @error != ''
+            erb :visit
+      else
+        dbase = SQLite3::Database.new 'public/customers.db' 
+       # dbase.execute "create table 'customers' ('id' integer primary key autoincrement, 'Name' varchar, 'ArDate' varchar, 'Phone' integer, 'Barber' varchar, 'Color' varchar)"
+        dbase.execute "insert into customers (Name, ArDate, Phone, Barber, Color) values ('#{@customer}', '#{@date}', '#{@phone}', '#{@barber}', '#{@color}')"
+
+       dbase.close
+       
+       erb "Your data are: #{@customer} | #{@date} | #{@phone} | #{@barber} | #{@color}\n"
+      end 
+  erb :visit
+end
+
+post '/contacts' do
+  @email = params[:email]
+  @report = params[:report]
+    dh = {
+        :email=>'Enter your email', 
+        :report=>'Enter your message' 
+          }
+        @error = dh.select {|key, value| params[key]==""}.values.join(", ")
+      if @error != ''
+            erb :contacts
+      else
+      messages = File.open 'public/messages.txt', 'a'
+      messages.puts "#{@email} | #{@report}\n"
+      messages.close
+
+      Pony.mail(:to => 'koq@nextmail.ru', :via => :sendmail, :from => "#{@email}", :body => "#{@report}" )
+      erb "<p>Спасибо за ваш отзыв</p> <a href='/'>HOME</a>"
+
+      end 
+
+  end
+
+get '/admen' do
+  erb :admen
+end
+
+post '/admen' do
+  
+  @lohin = params[:lohin]
+  @parol = params[:parol]
+
+  if @lohin == 'admin' && @parol == 'admin'
+
+  f = File.open 'public/customers.txt', 'r'
+  @listok = f.read
+  f.close
+  else
+    @irror = 'Access denied'
+  end
+end
